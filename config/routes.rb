1a3422034aad4702b03ac4be498213f03338f439
gibson2 <gibsonguillermoarturo@gmail.com>
2026-05-05 15:48:45 -0400

    Proyecto inicial de Ruby on Rails PruebaAI

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'about/index'
+  get 'products/index'
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "home#index"
+end
